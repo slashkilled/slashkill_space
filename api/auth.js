176c8b942da4572274d2ae8e3638a7c35774976c
slashkilled <nxnikitatox@gmail.com>
2026-08-21 20:48:26 +0300

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,4 +1,4 @@
 export default function handler(req, res) {
-  const url = `https://github.com/login/oauth/authorize?client_id=${process.env.OAUTH_CLIENT_ID}&scope=repo,user`;
+  const url = `https://github.com/login/oauth/authorize?client_id=${process.env.OAUTH_CLIENT_ID}&scope=repo%20user`;
   res.redirect(url);
 }
